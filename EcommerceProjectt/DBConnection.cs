7143526399d71309c08f492f401e7d3797e36eff
Ittyab Ur Rehman <ittyaboffical@gmail.com>
2023-07-09 16:15:32 +0500

    some fixes

--- EcommerceProjectt/DBConnection.cs
+++ EcommerceProjectt/DBConnection.cs
@@ -10,7 +10,7 @@ namespace EcommerceProjectt
          public static string server = "localhost";
          public static string database = "ecommercewebproject";
          public static string username = "root";
-         public static string password = "Nayab10033";
+         public static string password = "root";
                        
          public static string conString = "server=" + server + ";" + "database=" + database + ";" + "uid=" + username + ";" + "password=" + password + ";";
     }                  
